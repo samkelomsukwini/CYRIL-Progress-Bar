6ee2b2c23f9e9f2904480a5d1da3c39d5b6093e1
Samkelo Kgosiemang <joymama078@gmail.com>
2020-08-30 01:40:23 +0200

    Move access rokens to seperate file

--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -1,37 +1,48 @@
 import tweepy
 import datetime
 
-consumer_key = CKEY
-consumer_secret = CKEYSECRET
-access_token = AT
-access_token_secret = ATSECRET
+import config
+
+consumer_key = config.CONSUMER_KEY
+consumer_secret = config.CONSUMER_SECRET
+access_token = config.ACCESS_TOKEN
+access_token_secret = config.ACCESS_TOKEN_SECRET
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-
-def main():
-    inaug = datetime.date(2017, 1, 20)
+def bot():
+    inaug = datetime.date(2019, 5, 25)
     today = datetime.date.today()
     diff = today - inaug
+    
     numDiff = float(diff.days)
     yesterDiff = float(numDiff - 1) #will break on inaugeration day
     delta1 = numDiff / 1461
     delta2 = yesterDiff / 1461
     per1 = int(delta1 * 100)
     per2 = int(delta2 * 100)
+    
     print("Yesterday: ")
     print(per2)
     print("Today: ")
     print(per1)
-    if per1 != per2:
-        numXs = per1 // 4
-        numSpaces = 25 - numXs
-        c = "[" + ("x" * numXs) + ("_" * numSpaces) + "]"
-        tweetString = "The president's term is " + str(per1) + "% complete\n" + c
-        api.update_status(tweetString)
+
+    # if per1 != per2:
+    numXs = per1 // 4
+    numSpaces = 25 - numXs
+    c = ("▓" * numXs) + ("░" * numSpaces)
+    tweetString = "The president's term is " + str(per1) + "% complete\n" + c
+    
+    try:
+        api.update_status(tweetString+'\n'+'\n'+'#cyril #ancmustfall #southafrica')
+    except:
+        print('Error')
+
+def main():
+    bot()
 
 
 if __name__ == "__main__":
